f0c21e1f8335a1b58230594f5d580f524fd8d8ff
Pascal Thelke <p.thelke@googlemail.com>
2024-01-10 19:01:13 +0100

    general settings

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let fields = [
-    null,
+    'circle',
     null,
     null,
     'cross',
@@ -29,7 +29,7 @@ function render() {
         let cellContent = '';
   
         if (fields[index] === 'circle') {
-          cellContent = 'O';
+          cellContent = generateAnimatedCircleSVG();
         } else if (fields[index] === 'cross') {
           cellContent = 'X';
         }
@@ -43,5 +43,24 @@ function render() {
   
     // Füge den HTML-String zur innerHTML des 'content'-Elements hinzu
     content.innerHTML = tableHTML;
+  }
+
+  function generateAnimatedCircleSVG() {
+    const color = '#00B0EF';
+    const width = 70;
+    const height = 70;
+  
+    // SVG-HTML-Code für einen animierten Kreis
+    const svgCode = `
+      <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="${width / 2}" cy="${height / 2}" r="${width / 2 - 2}" fill="none" stroke="${color}" stroke-width="4">
+          <animate attributeName="r" values="0;${width / 2 - 2}" 
+           begin="0s" repeatCount="indefinite" />
+          <animate attributeName="stroke-dasharray" values="0 ${2 * Math.PI * (width / 2 - 2)};0 0" dur="1s" begin="0s" repeatCount="indefinite" />
+        </circle>
+      </svg>
+    `;
+  
+    return svgCode;
   }
   
